d8efda4e6888165454992173993428b4b260d48d
SathishBublu <sathishvm37@gmail.com>
2021-05-14 22:58:25 +0530

    add: alert, spinner component

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // bootstrap
 
-// import 'bootstrap/js/dist/alert';
-// import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/button';
 // import 'bootstrap/js/dist/carousel';
 import 'bootstrap/js/dist/collapse';
 import 'bootstrap/js/dist/dropdown';
